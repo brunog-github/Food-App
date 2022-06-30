e189a47ee9c586831963d2ae0b9e1ae05ea1bbde
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2022-06-30 13:48:03 -0300

    terminado as configurações do dataStore e guardando as preferencias do usuarios, usando safe args na navegação.

--- app/src/main/java/com/example/foodapp/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/recipes/RecipesFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodapp.R
 import com.example.foodapp.viewmodels.MainViewModel
@@ -31,6 +32,8 @@ class RecipesFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val adapter by lazy { RecipesAdapter() }
+
+    private val args by navArgs<RecipesFragmentArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,7 +69,7 @@ class RecipesFragment : Fragment() {
     private fun readDatabase() {
         lifecycleScope.launch {
             mainViewModel.readRecipes.observeOnce(viewLifecycleOwner) { database ->
-                if (database.isNotEmpty()) {
+                if (database.isNotEmpty() && !args.backFromBottomSheet) {
                     adapter.setData(database[0].foodRecipe)
                     hideShimmerEffect()
                 } else {
--- app/src/main/java/com/example/foodapp/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foodapp/viewmodels/RecipesViewModel.kt
@@ -2,6 +2,8 @@ package com.example.foodapp.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodapp.data.DataStoreRepository
 import com.example.foodapp.util.Constants.API_KEY
 import com.example.foodapp.util.Constants.DEFAULT_DIET_TYPE
 import com.example.foodapp.util.Constants.DEFAULT_MEAL_TYPE
@@ -13,20 +15,47 @@ import com.example.foodapp.util.Constants.QUERY_FILL_INGREDIENTS
 import com.example.foodapp.util.Constants.QUERY_NUMBER
 import com.example.foodapp.util.Constants.QUERY_TYPE
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipesViewModel @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository,
     application: Application
-): AndroidViewModel(application) {
+) : AndroidViewModel(application) {
 
-     fun applyQueries(): HashMap<String, String> {
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(
+        mealType: String,
+        mealTypeId: Int,
+        dietType: String,
+        dietTypeId: Int
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
+    }
+
+    fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
 
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_APIKEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
