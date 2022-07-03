6fc6b91234ff7bca9f5c7d395426fe5b1857834a
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2022-07-03 15:01:02 -0300

    implementado o webview e terminado a InstructionsFragment.kt.

--- app/src/main/java/com/example/foodapp/ui/fragments/details/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/details/InstructionsFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foodapp.R
+import com.example.foodapp.databinding.FragmentInstructionsBinding
+import com.example.foodapp.models.Result
+import com.example.foodapp.util.Constants
+import java.util.*
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+
+        return binding.root
     }
 }
